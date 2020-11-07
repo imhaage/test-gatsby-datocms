d7c7b0c55b96d8f6a8ea82bc58c8f0fbeed9acc1
Mathieu HAAGE <mahaage@gmail.com>
2020-11-07 16:34:38 +0100

    Replace marked by react-markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,25 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import ReactMarkdown from "react-markdown"
 import Layout from "../components/layout"
 
 const IndexPage = ({ data }) => {
-  console.log(data)
   const { datoCmsPage } = data
+  console.log(data)
   return (
     <Layout>
       <h1>{datoCmsPage.titreDeLaPage}</h1>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: datoCmsPage.texteBloc1.fields.html,
-        }}
-      />
+      <p>
+        <ReactMarkdown>{datoCmsPage.texteBloc1}</ReactMarkdown>
+      </p>
       <h2>{datoCmsPage.titreBloc2}</h2>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: datoCmsPage.texteBloc2.fields.html,
-        }}
-      />
+      <p>
+        <ReactMarkdown>{datoCmsPage.texteBloc2}</ReactMarkdown>
+      </p>
       <h2>{datoCmsPage.titreBloc3}</h2>
-      <p
-        dangerouslySetInnerHTML={{
-          __html: datoCmsPage.texteBloc3.fields.html,
-        }}
-      ></p>
+      <p>
+        <ReactMarkdown>{datoCmsPage.texteBloc3}</ReactMarkdown>
+      </p>
     </Layout>
   )
 }
@@ -34,23 +28,12 @@ export const query = graphql`
   query HomePageQuery {
     datoCmsPage {
       titreDeLaPage
-      texteBloc1: texteBloc1Node {
-        fields {
-          html
-        }
-      }
+      titreBloc1
+      texteBloc1
       titreBloc2
-      texteBloc2: textBloc2Node {
-        fields {
-          html
-        }
-      }
+      textBloc2
       titreBloc3
-      texteBloc3: texteBloc3Node {
-        fields {
-          html
-        }
-      }
+      texteBloc3
     }
   }
 `
