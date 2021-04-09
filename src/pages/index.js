45bb818c781c3d23f41fa1fcda227fea4bcfe37d
Mathieu HAAGE <mahaage@gmail.com>
2021-04-10 00:56:47 +0200

    Improve price table

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,48 +7,70 @@ import "@fontsource/noto-serif"
 import "@fontsource/noto-serif/400-italic.css"
 import "@fontsource/noto-serif/700.css"
 import "@fontsource/kaushan-script"
+import "./app.css"
 
 const IndexPage = ({ data }) => {
   const { dato, gallery } = data
 
   return (
     <Layout>
-      <h1>{dato.titreDeLaPage}</h1>
-      <p>
-        <ReactMarkdown>{dato.texteBloc1}</ReactMarkdown>
-      </p>
-      <h2>{dato.titreBloc2}</h2>
-      <p>
-        <ReactMarkdown>{dato.texteBloc2}</ReactMarkdown>
-      </p>
-      <h2>{dato.titreBloc3}</h2>
-      <p>
-        <ReactMarkdown>{dato.texteBloc3}</ReactMarkdown>
-      </p>
-      <p>
-        <h2>Tarifs</h2>
-        <ul>
-          <li>Prix 1 : {dato.prix1} €</li>
-          <li>Prix 2 : {dato.prix2} €</li>
-          <li>Prix 3 : {dato.prix3} €</li>
-          <li>Prix 4 : {dato.prix4} €</li>
-        </ul>
-      </p>
+      <section>
+        <h1>{dato.titreDeLaPage}</h1>
+        <p>
+          <ReactMarkdown>{dato.texteBloc1}</ReactMarkdown>
+        </p>
+        <h2>{dato.titreBloc2}</h2>
+        <p>
+          <ReactMarkdown>{dato.texteBloc2}</ReactMarkdown>
+        </p>
+        <h2>{dato.titreBloc3}</h2>
+        <p>
+          <ReactMarkdown>{dato.texteBloc3}</ReactMarkdown>
+        </p>
+      </section>
 
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-        }}
-      >
-        {gallery.photos.map(photo => (
-          <Img
-            style={{ width: "calc(33.33% - 0.25rem)", marginBottom: "0.5rem" }}
-            fluid={photo.fluid}
-          />
-        ))}
-      </div>
+      <section>
+        <h2>Tarifs</h2>
+        <table style={{ textAlign: "center" }}>
+          <thead>
+            <tr>
+              <th>Prix 1</th>
+              <th>Prix 2</th>
+              <th>Prix 3</th>
+              <th>Prix 4</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{dato.prix1}</td>
+              <td>{dato.prix2}</td>
+              <td>{dato.prix3}</td>
+              <td>{dato.prix4}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section>
+        <h2>Gallerie</h2>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          {gallery.photos.map(photo => (
+            <Img
+              style={{
+                width: "calc(33.33% - 0.25rem)",
+                marginBottom: "0.5rem",
+              }}
+              fluid={photo.fluid}
+            />
+          ))}
+        </div>
+      </section>
     </Layout>
   )
 }
